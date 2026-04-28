c52992c84496e376b4ca23718aa1351a5aaed805
Mostafa Ali <captain.desha55@gmail.com>
2026-04-28 20:34:38 +0300

    feat: secure dashboard with login_required decorator

--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -7,7 +8,7 @@ def home(request):
     context = {'home':home}
     return render(request, 'web/home.html', context)
 
-
+@login_required(login_url='users:login')
 def dashboard(request):
     context = {'dashboard':dashboard}
     return render(request, 'web/dashboard.html', context)
